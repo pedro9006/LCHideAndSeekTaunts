724d16fccda09ab12bed0efd8344ad38efd54e83
SpLinTRTV <At0m3_SpLinT3r@hotmail.fr>
2024-11-10 23:00:13 +0100

    Add files via upload

---
+++ Taunt_FR/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Taunt_FR
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Taunt_FR";
+        public const string PLUGIN_NAME = "Taunt Hide and Seek FR";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
