2896333d7e7cccc27ff3662e84f57a29a64a1e1c
pedro9006 <83221633+pedro9006@users.noreply.github.com>
2024-12-02 17:54:51 -0500

    Update MyPluginInfo.cs

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -2,8 +2,8 @@ namespace Taunt_FR
 {
     public static class MyPluginInfo
     {
-        public const string PLUGIN_GUID = "Taunt_FR";
-        public const string PLUGIN_NAME = "Taunt Hide and Seek FR";
+        public const string PLUGIN_GUID = "Taunt_FATCOOP";
+        public const string PLUGIN_NAME = "Taunt Hide and Seek FATCOOP";
         public const string PLUGIN_VERSION = "1.0.0";
     }
 }
